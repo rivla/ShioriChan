bf3d97a3f0d8262c125596b4e6201dd0b2d52baf
rivla <kazuaki@asr.info.gifu-u.ac.jp>
2017-10-05 18:02:50 +0900

    jsonファイルの読み込みを追加

---
+++ app/src/main/java/jp/ncf/app/shiorichan/JsonReader.java
@@ -0,0 +1,52 @@
+package jp.ncf.app.shiorichan;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+
+/**
+ * Created by kazu on 17/10/05.
+ */
+public class JsonReader {
+
+    static public JSONObject ReadJson(Context csContext, String strFileName)
+    {
+        InputStream inputStream = null;
+        BufferedReader bufferedReader = null;
+
+        JSONObject json = null;
+        try {
+            // 事前に用意しておいた、jsonファイルを読み込みます
+            AssetManager csAsset = csContext.getResources().getAssets();
+            inputStream = csAsset.open(strFileName);
+            bufferedReader =
+                    new BufferedReader(new InputStreamReader(inputStream));
+            String str = bufferedReader.readLine();
+
+            // JSONObject に変換します
+            json = new JSONObject(str);
+
+            // JSONObject を文字列に変換してログ出力します
+//            Log.d("json test", json.toString(4));
+
+            inputStream.close();
+            bufferedReader.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return json;
+    }
+}
